eb40646b82401966c4a59144af58d900d6444cdb
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-04-23 08:31:15 +0100

    sbuf: Introduce a (mono) Filter trait
    In addition to the basic trait (and the process loop) we also use
    the trait to migrate some helper functions out of the unit tests.
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/sbuf.rs
+++ src/sbuf.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2022 Daniel Thompson
 
+use crate::*;
+use std::iter::zip;
+
+// TODO: replace with type declaration
 #[derive(Debug, Default)]
 pub struct SampleBuffer {
     pub v: Vec<f32>,
@@ -44,6 +48,37 @@ pub trait SignalGenerator {
         for spl in samples {
             *spl = self.step();
         }
+    }
+}
+
+pub trait Filter {
+    fn step(&mut self, spl: f32) -> f32;
+    fn flush(&mut self);
+
+    fn process(&mut self, inbuf: &[f32], outbuf: &mut [f32]) {
+        for (inspl, outspl) in zip(inbuf, outbuf) {
+            *outspl = self.step(*inspl);
+        }
+    }
+
+    /// Stimulate the filter with a specific pure-sine wave.
+    ///
+    /// TODO: *Does this really need to be a method?*
+    fn stimulate(&mut self, ctx: &AudioContext, gfreq: i32) -> f32 {
+        let mut inbuf = [0.0_f32; 1024];
+        let mut outbuf = [0.0_f32; 1024];
+        let mut sg = SineGenerator::default();
+
+        sg.setup(&ctx, gfreq, 1.570793);
+
+        // stimulate the filter
+        for _ in 0..10 {
+            sg.process(&mut inbuf);
+            self.process(&inbuf, &mut outbuf);
+        }
+
+        // check the result
+        outbuf.analyse_rectify()
     }
 }
 
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2022 Daniel Thompson
 
+use crate::*;
+
 #[derive(Clone, Copy, Debug, Default)]
 pub struct AudioContext {
     pub sampling_frequency: i32,
@@ -45,4 +47,43 @@ pub fn db2linear(db: f32) -> f32 {
 
 pub fn linear2db(spl: f32) -> f32 {
     spl.log10() * 20.0
+}
+
+pub fn check_response(ctx: &AudioContext, f: &mut impl Filter, gfreq: i32, db: f32) -> bool {
+    let level = f.stimulate(ctx, gfreq);
+    let ok;
+
+    if 0.0 == db {
+        ok = fuzzcmp(level, 1.0, 1.05);
+
+        println!(
+            "    {:4}Hz@{}Hz    {:6.2} {}=   1.00              [ {} ]",
+            gfreq,
+            ctx.sampling_frequency,
+            level,
+            if ok { '~' } else { '!' },
+            if ok { " OK " } else { "FAIL" }
+        );
+    } else {
+        let dblevel = linear2db(level);
+
+        /* special case for very quiet signals */
+        if db <= -96.0 {
+            ok = dblevel <= (db + 6.0);
+        } else {
+            ok = fuzzcmp(dblevel, db, 1.1);
+        }
+
+        println!(
+            "    {:4}Hz@{}Hz    {:6.2} {}= {:6.2}dB            [ {} ]",
+            gfreq,
+            ctx.sampling_frequency,
+            dblevel,
+            if ok { '~' } else { '!' },
+            db,
+            if ok { " OK " } else { "FAIL" }
+        );
+    }
+
+    ok
 }
