164a3317b998c387ec0664d4e83c1a4facd1b45e
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-04-26 20:09:53 +0100

    tubestage: Add basic unit tests
    These tests show that the first tubestage is much less stable than the
    libtt equivalent. This is probably OK when we use the tubestage in a
    preamp model (e.g. after any distortion effects).
    
    However... at some point this will need a bit more attention paying to it.
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/tubestage.rs
+++ src/tubestage.rs
@@ -17,6 +17,7 @@ pub enum Tube {
     Tube12AX7Ri250K,
 }
 
+#[derive(Debug, Default)]
 pub struct TubeStage<'a> {
     vp: f32,
     predivide: f32,
@@ -81,5 +82,83 @@ impl<'a> Filter for TubeStage<'a> {
         self.input_filter.flush();
         self.feedback_filter.flush();
         self.output_filter.flush();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn settle(ts: &mut TubeStage, outbuf: &mut [f32]) {
+        let inbuf = [0.0; 256];
+
+        let _ = ts.step(1.0);
+        for _ in 0..25 {
+            ts.process(&inbuf, outbuf);
+        }
+
+        let peak = linear2db(outbuf.analyse_peak());
+        let rectify = linear2db(outbuf.analyse_rectify());
+        println!("peak {:6.2}db  rectify {:6.2}db", peak, rectify);
+        assert!(peak < -95.0); // -96.0?
+        assert!(rectify < -96.0);
+    }
+
+    fn stimulate(ctx: &AudioContext, ts: &mut TubeStage, outbuf: &mut [f32]) {
+        let mut inbuf = [0.0; 256];
+        let mut sg = SineGenerator::default();
+        sg.setup(ctx, 200, db2linear(-12.0));
+        for _ in 0..2 {
+            sg.process(&mut inbuf);
+            ts.process(&inbuf, outbuf);
+        }
+    }
+
+    #[test]
+    fn test_stage1() {
+        let ctx = AudioContext::new(48000);
+        let mut outbuf = [0.0; 256];
+        let mut ts = TubeStage::default();
+        ts.setup(&ctx, Tube::Tube12AX7Ri68K, 0.0, 2700.0, 22570, 86);
+
+        settle(&mut ts, &mut outbuf);
+        stimulate(&ctx, &mut ts, &mut outbuf);
+        let peak = linear2db(outbuf.analyse_peak());
+        let rectify = linear2db(outbuf.analyse_rectify());
+        println!("peak {:7.2}db  rectify {:7.2}db\n", peak, rectify,);
+        assert!(peak > -12.0);
+        assert!(rectify > -18.0);
+    }
+
+    #[test]
+    fn test_stage2() {
+        let ctx = AudioContext::new(48000);
+        let mut outbuf = [0.0; 256];
+        let mut ts = TubeStage::default();
+        ts.setup(&ctx, Tube::Tube12AX7Ri250K, 0.0, 1500.0, 6531, 132);
+
+        settle(&mut ts, &mut outbuf);
+        stimulate(&ctx, &mut ts, &mut outbuf);
+        let peak = linear2db(outbuf.analyse_peak());
+        let rectify = linear2db(outbuf.analyse_rectify());
+        println!("peak {:7.2}db  rectify {:7.2}db\n", peak, rectify,);
+        assert!(peak > -12.0);
+        assert!(rectify > -12.0);
+    }
+
+    #[test]
+    fn test_stage3() {
+        let ctx = AudioContext::new(48000);
+        let mut outbuf = [0.0; 256];
+        let mut ts = TubeStage::default();
+        ts.setup(&ctx, Tube::Tube12AX7Ri250K, 0.0, 820.0, 6531, 194);
+
+        settle(&mut ts, &mut outbuf);
+        stimulate(&ctx, &mut ts, &mut outbuf);
+        let peak = linear2db(outbuf.analyse_peak());
+        let rectify = linear2db(outbuf.analyse_rectify());
+        println!("peak {:7.2}db  rectify {:7.2}db\n", peak, rectify,);
+        assert!(peak > -12.0);
+        assert!(rectify > -12.0);
     }
 }
