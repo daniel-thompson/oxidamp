cd2e2bb82dcbec717d183f8f3fcfe243dd1b0135
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-05-04 21:38:15 +0100

    benchmark: Increase the number of filters covered
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- benches/main.rs
+++ benches/main.rs
@@ -8,18 +8,54 @@ use bencher::Bencher;
 use oxidamp::*;
 use rand::prelude::*;
 
-fn randbuf() -> [f32; 1024] {
+fn randbuf<const L: usize>() -> [f32; L] {
     let mut rng = thread_rng();
-    let mut inbuf = [0.0_f32; 1024];
+    let mut inbuf = [0.0_f32; L];
     for i in &mut inbuf {
         *i = rng.gen_range(-1.0..1.0);
     }
     inbuf
 }
 
-fn fir16(bench: &mut Bencher) {
-    let inbuf = randbuf();
-    let mut outbuf = [0.0; 1024];
+fn amplifier<const L: usize>(bench: &mut Bencher) {
+    let inbuf = randbuf::<L>();
+    let mut outbuf = [0.0; L];
+    let ctx = AudioContext::new(48000);
+    let mut amp = Amplifier::default();
+    amp.setup(&ctx);
+
+    bench.iter(|| {
+        amp.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn biquad<const L: usize>(bench: &mut Bencher) {
+    let inbuf = randbuf::<L>();
+    let mut outbuf = [0.0; L];
+    let ctx = AudioContext::new(48000);
+    let mut bq = Biquad::default();
+    bq.lowpass(&ctx, 12000, 0.7);
+
+    bench.iter(|| {
+        bq.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn cabsim<const L: usize>(bench: &mut Bencher) {
+    let inbuf = randbuf::<L>();
+    let mut outbuf = [0.0; L];
+    let ctx = AudioContext::new(48000);
+    let mut cabsim = CabinetSimulator::default();
+    cabsim.setup(&ctx);
+
+    bench.iter(|| {
+        cabsim.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn fir16<const L: usize>(bench: &mut Bencher) {
+    let inbuf = randbuf::<L>();
+    let mut outbuf = [0.0; L];
     let mut fir = fir16_halfband();
 
     bench.iter(|| {
@@ -27,9 +63,9 @@ fn fir16(bench: &mut Bencher) {
     });
 }
 
-fn fir64(bench: &mut Bencher) {
-    let inbuf = randbuf();
-    let mut outbuf = [0.0; 1024];
+fn fir64<const L: usize>(bench: &mut Bencher) {
+    let inbuf = randbuf::<L>();
+    let mut outbuf = [0.0; L];
     let mut fir = fir64_halfband();
 
     bench.iter(|| {
@@ -37,5 +73,29 @@ fn fir64(bench: &mut Bencher) {
     });
 }
 
-benchmark_group!(benches, fir16, fir64);
+benchmark_group!(
+    benches,
+    // This is a very special case! 480 is both a sensibly
+    // small chunk size *and* represents ~10ms of samples
+    // at 48000Hz. Dividing the result of this test by 10000
+    // yields the cost in milliseconds to process a seconds
+    // worth of samples.
+    amplifier::<480>,
+
+    amplifier::<256>,
+    amplifier::<1024>,
+    amplifier::<4096>,
+    biquad::<256>,
+    biquad::<1024>,
+    biquad::<4096>,
+    cabsim::<256>,
+    cabsim::<1024>,
+    cabsim::<4096>,
+    fir16::<256>,
+    fir16::<1024>,
+    fir16::<4096>,
+    fir64::<256>,
+    fir64::<1024>,
+    fir64::<4096>
+);
 benchmark_main!(benches);
