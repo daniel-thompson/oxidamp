198c6213e3f96c92ab2b39e8df191096213ee0f9
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-05-04 22:16:43 +0100

    benchmark: Better coverage, chunk sizes and initialization
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- benches/main.rs
+++ benches/main.rs
@@ -17,10 +17,15 @@ fn randbuf<const L: usize>() -> [f32; L] {
     inbuf
 }
 
-fn amplifier<const L: usize>(bench: &mut Bencher) {
-    let inbuf = randbuf::<L>();
-    let mut outbuf = [0.0; L];
+fn init<const L: usize>() -> (AudioContext, [f32; L], [f32; L]) {
     let ctx = AudioContext::new(48000);
+    let inbuf = randbuf::<L>();
+    let outbuf = [0.0; L];
+    (ctx, inbuf, outbuf)
+}
+
+fn amplifier<const L: usize>(bench: &mut Bencher) {
+    let (ctx, inbuf, mut outbuf) = init::<L>();
     let mut amp = Amplifier::default();
     amp.setup(&ctx);
 
@@ -30,9 +35,7 @@ fn amplifier<const L: usize>(bench: &mut Bencher) {
 }
 
 fn biquad<const L: usize>(bench: &mut Bencher) {
-    let inbuf = randbuf::<L>();
-    let mut outbuf = [0.0; L];
-    let ctx = AudioContext::new(48000);
+    let (ctx, inbuf, mut outbuf) = init::<L>();
     let mut bq = Biquad::default();
     bq.lowpass(&ctx, 12000, 0.7);
 
@@ -42,9 +45,7 @@ fn biquad<const L: usize>(bench: &mut Bencher) {
 }
 
 fn cabsim<const L: usize>(bench: &mut Bencher) {
-    let inbuf = randbuf::<L>();
-    let mut outbuf = [0.0; L];
-    let ctx = AudioContext::new(48000);
+    let (ctx, inbuf, mut outbuf) = init::<L>();
     let mut cabsim = CabinetSimulator::default();
     cabsim.setup(&ctx);
 
@@ -54,8 +55,7 @@ fn cabsim<const L: usize>(bench: &mut Bencher) {
 }
 
 fn fir16<const L: usize>(bench: &mut Bencher) {
-    let inbuf = randbuf::<L>();
-    let mut outbuf = [0.0; L];
+    let (_, inbuf, mut outbuf) = init::<L>();
     let mut fir = fir16_halfband();
 
     bench.iter(|| {
@@ -64,8 +64,7 @@ fn fir16<const L: usize>(bench: &mut Bencher) {
 }
 
 fn fir64<const L: usize>(bench: &mut Bencher) {
-    let inbuf = randbuf::<L>();
-    let mut outbuf = [0.0; L];
+    let (_, inbuf, mut outbuf) = init::<L>();
     let mut fir = fir64_halfband();
 
     bench.iter(|| {
@@ -73,29 +72,74 @@ fn fir64<const L: usize>(bench: &mut Bencher) {
     });
 }
 
+fn preamp<const L: usize>(bench: &mut Bencher) {
+    let (ctx, inbuf, mut outbuf) = init::<L>();
+    let mut preamp = Preamp::default();
+    preamp.setup(&ctx);
+
+    bench.iter(|| {
+        preamp.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn tonestack<const L: usize>(bench: &mut Bencher) {
+    let (ctx, inbuf, mut outbuf) = init::<L>();
+    let mut ts = ToneStack::default();
+    ts.setup(&ctx);
+
+    bench.iter(|| {
+        ts.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn tubestage<const L: usize>(bench: &mut Bencher) {
+    let (ctx, inbuf, mut outbuf) = init::<L>();
+    let mut ts = TubeStage::default();
+    ts.setup(&ctx, Tube::Tube12AX7Ri68K, 0.0, 2700.0, 22570, 86);
+
+    bench.iter(|| {
+        ts.process(&inbuf, &mut outbuf);
+    });
+}
+
+fn waveshaper<const L: usize>(bench: &mut Bencher) {
+    let (ctx, inbuf, mut outbuf) = init::<L>();
+    let shape = [-1.0, -0.875, -0.75, -0.5, 0.0, 0.5, 0.75, 0.875, 1.0];
+    let mut shaper = WaveShaper::default();
+    shaper.setup(&ctx, -2.0, 2.0, &shape);
+
+    bench.iter(|| {
+        shaper.process(&inbuf, &mut outbuf);
+    });
+}
+
+//
+// Why 480? It's a both sensible chunk size for low latency use *and* it
+// represents ~10ms of samples at 48000Hz. This dividing the result of this
+// test by 10000 yields the cost in milliseconds to process a seconds worth of
+// samples.
+//
+// Why 1920? It's like 480 but gives benchmarks for x4 oversampling ;-)
+//
 benchmark_group!(
     benches,
-    // This is a very special case! 480 is both a sensibly
-    // small chunk size *and* represents ~10ms of samples
-    // at 48000Hz. Dividing the result of this test by 10000
-    // yields the cost in milliseconds to process a seconds
-    // worth of samples.
+    amplifier::<1920>,
     amplifier::<480>,
-
-    amplifier::<256>,
-    amplifier::<1024>,
-    amplifier::<4096>,
-    biquad::<256>,
-    biquad::<1024>,
-    biquad::<4096>,
-    cabsim::<256>,
-    cabsim::<1024>,
-    cabsim::<4096>,
-    fir16::<256>,
-    fir16::<1024>,
-    fir16::<4096>,
-    fir64::<256>,
-    fir64::<1024>,
-    fir64::<4096>
+    biquad::<1920>,
+    biquad::<480>,
+    cabsim::<1920>,
+    cabsim::<480>,
+    fir16::<1920>,
+    fir16::<480>,
+    fir64::<1920>,
+    fir64::<480>,
+    preamp::<1920>,
+    preamp::<480>,
+    tonestack::<1920>,
+    tonestack::<480>,
+    tubestage::<1920>,
+    tubestage::<480>,
+    waveshaper::<1920>,
+    waveshaper::<480>,
 );
 benchmark_main!(benches);
