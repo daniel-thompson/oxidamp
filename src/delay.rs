5bd7218d01e65e2f65c81b31a826219cd6413644
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-05-22 20:24:56 +0100

    delay: Add unit tests (and resulting fixes)
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/delay.rs
+++ src/delay.rs
@@ -15,7 +15,7 @@ impl<const L: usize> Default for Delay<L> {
         Self {
             buf: [0.0; L],
             insert_at: 0,
-            extract_from: L - 1,
+            extract_from: 0,
         }
     }
 }
@@ -23,10 +23,10 @@ impl<const L: usize> Default for Delay<L> {
 impl<const L: usize> Delay<L> {
     pub fn setup(&mut self, _ctx: &AudioContext, n: usize) {
         let len = self.buf.len();
-        debug_assert!(n < len);
+        debug_assert!(n > 0 && n <= len);
 
-        self.extract_from = self.insert_at + n;
-        if self.extract_from > len {
+        self.extract_from = self.insert_at + len - n;
+        if self.extract_from >= len {
             self.extract_from -= len;
         }
     }
@@ -58,6 +58,64 @@ impl<const L: usize> Filter for Delay<L> {
     fn flush(&mut self) {
         for spl in &mut self.buf {
             *spl = 0.0;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default_delay() {
+        let mut delay = Delay::<10>::default();
+
+        for i in -10..11 {
+            let f = i as f32 / 10.0;
+
+            if i < 0 {
+                assert_eq!(0.0, delay.step(f));
+            } else {
+                let z = (i - 10) as f32 / 10.0;
+                assert_eq!(z, delay.step(f));
+            }
+        }
+    }
+
+    #[test]
+    fn test_delay_one() {
+        let mut delay = Delay::<10>::default();
+        let ctx = AudioContext::new(48000);
+        delay.setup(&ctx, 1);
+
+        for i in -10..11 {
+            let f = i as f32 / 10.0;
+
+            println!("i {}  f {}", i, f);
+            if i < -9 {
+                assert_eq!(0.0, delay.step(f));
+            } else {
+                let z = (i - 1) as f32 / 10.0;
+                assert_eq!(z, delay.step(f));
+            }
+        }
+    }
+
+    #[test]
+    fn test_delay_10() {
+        let ctx = AudioContext::new(48000);
+        let mut delay = Delay::<20>::default();
+        delay.setup(&ctx, 10);
+
+        for i in -10..11 {
+            let f = i as f32 / 10.0;
+
+            if i < 0 {
+                assert_eq!(0.0, delay.step(f));
+            } else {
+                let z = (i - 10) as f32 / 10.0;
+                assert_eq!(z, delay.step(f));
+            }
         }
     }
 }
