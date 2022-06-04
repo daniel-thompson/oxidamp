1f70f450d2b2c681b600d7f7be9746691e9c5f69
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-06-04 21:56:10 +0100

    midi: Add support for NoteOn/Off events
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/midi.rs
+++ src/midi.rs
@@ -1,36 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2022 Daniel Thompson
 
-const MAX_MIDI: usize = 3;
+use jack::RawMidi;
+use std::convert::From;
 
-// a fixed size container to copy data out of real-time thread
-#[derive(Copy, Clone)]
-pub struct MidiEvent {
-    len: usize,
-    data: [u8; MAX_MIDI],
-    time: u32,
+#[derive(Copy, Clone, Debug)]
+pub struct MidiNote {
+    pub note: u8,
+    pub velocity: u8,
 }
 
-impl std::convert::From<jack::RawMidi<'_>> for MidiEvent {
-    fn from(midi: jack::RawMidi<'_>) -> Self {
-        let len = std::cmp::min(MAX_MIDI, midi.bytes.len());
-        let mut data = [0; MAX_MIDI];
-        data[..len].copy_from_slice(&midi.bytes[..len]);
-        MidiEvent {
-            len,
-            data,
-            time: midi.time,
-        }
+impl MidiNote {
+    pub fn new(note: u8, velocity: u8) -> Self {
+        Self { note, velocity }
     }
 }
 
-impl std::fmt::Debug for MidiEvent {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "MidiEvent {{ time: {:4}, data: {:?} }}",
-            self.time,
-            &self.data[..self.len],
-        )
+#[derive(Copy, Clone, Debug)]
+pub enum MidiData {
+    NoteOn(MidiNote),
+    NoteOff(MidiNote),
+    Raw((u8, u8, u8)),
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct MidiEvent {
+    pub data: MidiData,
+    pub time: u32,
+}
+
+impl From<RawMidi<'_>> for MidiEvent {
+    fn from(midi: RawMidi<'_>) -> Self {
+        // Grab the first three bytes (or zero is packet too short)
+        let len = midi.bytes.len();
+        let d0 = if len > 0 { midi.bytes[0] } else { 0 };
+        let d1 = if len > 1 { midi.bytes[1] } else { 0 };
+        let d2 = if len > 2 { midi.bytes[2] } else { 0 };
+
+        // For now we can manage with a very simple midi implementation.
+        // We currently only pick out note on/off. For everything else
+        // we just capture the first three bytes and hope...
+        MidiEvent {
+            data: if d0 == 144 {
+                MidiData::NoteOn(MidiNote::new(d1, d2))
+            } else if d0 == 128 {
+                MidiData::NoteOff(MidiNote::new(d1, d2))
+            } else {
+                MidiData::Raw((d0, d1, d2))
+            },
+            time: midi.time,
+        }
     }
 }
