4a58229762a77103f0cb508de725584d3de15de2
Daniel Thompson <daniel@redfelineninja.org.uk>
2023-01-14 22:19:10 +0000

    lib: Add an overall doc comment
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2022 Daniel Thompson
 
+//! oxidamp - A Digital Amplifier in Rust
+//!
+//! oxidamp is a digital modelling amplifier written in Rust. It is currently
+//! in a nascent state. It currently implements a simple fixed-function mono
+//! signal chain that includes preamp, tone stack and cabinet simulation.
+
 pub mod amplifier;
 pub mod biquad;
 pub mod cabsim;
